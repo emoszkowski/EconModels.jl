da8cf123089d9acb26e1879e27e68f786a25cf4a
Erica Moszkowski <erica.moszkowski@gmail.com>
2017-02-23 09:44:41 -0500

    add additional convert test

--- test/settings.jl
+++ test/settings.jl
@@ -9,4 +9,5 @@ vint = Setting(:data_vintage, "REF", true, "vint", "Data vintage") # full constr
 @test promote_rule(Setting{Bool}, Bool) == Bool
 @test promote_rule(Setting{ASCIIString}, AbstractString) == UTF8String
 @test convert(Int64, test1) == 22
+@test convert(Float32, test1) == Float32(22.)
 @test convert(ASCIIString, vint) == "REF"
